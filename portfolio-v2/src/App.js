4af198fc3486bca36975c4e67700377959d9c12e
jstearne <jared.stearne@gmail.com>
2022-02-03 10:09:01 -0800

    Added background non-repeating with react styling. Need to 'raise' the image a bit to see the bottom of it, perhaps?

--- portfolio-v2/src/App.js
+++ portfolio-v2/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import background from "./img/background.jpg"; /* custom background image */
 import './App.css';
 
 // how to input background color here? I think this is correct (styles) but overwritten by standard CSS.
 function App() {
   return (
-    <div className="App" styles={{ backgroundColor:`red` }} >
+    <div className="App" style={{ backgroundImage:`url(${background})`, height:'auto', width:'auto', backgroundRepeat:'no-repeat', backgroundAttachment:'fixed', backgroundPosition:'center', }}  >
       <header className="App-header"> {/* this is setting css for App */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
